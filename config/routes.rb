d71528bb6c05f4acb9b6da84febd64f3bad13e32
ravenusmc <mcuddy77@gmail.com>
2016-04-12 14:54:45 -0400

    create comments model controller and view

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,10 @@
 Rails.application.routes.draw do
 
   devise_for :users
-  resources :messages
+  resources :messages do
+    resources :comments
+  end 
   root 'messages#index'
-
-
-
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
